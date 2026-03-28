ac4c2af901475c0e8b8854aa405671bad7fc1d16
AlyHacks <137717874+AlyHacks@users.noreply.github.com>
2026-03-27 22:44:59 -0400

    Update robot speed simulation from 5 m/s to 9 m/s

--- labs/lab_1/lab_1a.py
+++ labs/lab_1/lab_1a.py
@@ -14,4 +14,6 @@ def main():
 
 if __name__ == "__main__":
     main()
-This is to simulate a change made on a robot: robot_speed = 5 # m/s
+    
+#This is to simulate a change made on a robot: 
+robot_speed = 9 # m/s
